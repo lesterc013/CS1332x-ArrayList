92dbfc2b53a635643f6dfe3d961bf599b7919ec3
Lester Chan <lesterc013@gmail.com>
2024-06-11 20:48:16 +0800

    First commit - Created addToBack method

---
+++ Driver.java
@@ -0,0 +1,13 @@
+
+public class Driver {
+    public static void main(String[] args) {
+        ArrayList<Integer> test = new ArrayList<>();
+        for (int i = 1; i <= 36; i++) {
+            test.addToBack(i);
+        }
+        for (int i = 0; i < test.size(); i++) {
+            System.out.println(test.get(i));
+        }
+        test.addToBack(null);
+    }
+}
